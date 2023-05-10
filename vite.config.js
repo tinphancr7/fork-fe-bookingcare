f16b3583dc074a636e6d6420663bd1ef5d1676cc
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-10 09:21:26 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/",
 	server: {
 		port: 8000,
 	},
-	base: "{repName}",
 });
