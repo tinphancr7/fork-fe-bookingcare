71c8a4d3d9a8389b56c23c9e26e74290493801d6
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-26 16:29:00 +0700

    update

--- src/App copy.jsx
+++ src/App copy.jsx
@@ -37,7 +37,46 @@ const Profile = lazy(() => import("./pages/Profile"));
 function App() {
 	return (
 		<>
-			<div>hello</div>
+			<Suspense fallback={<Spinner />}>
+				<Routes>
+					<Route path="/" element={<MainLayout />}>
+						<Route index element={<Home />} />
+						<Route path="/signup" element={<SignupForm />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/unauthorized" element={<Unauthorize />} />
+						<Route element={<ProtectedRoute />}>
+							<Route path="/profile" element={<Profile />} />
+						</Route>
+
+						<Route path="/specialties/:id" element={<DetailSpecialty />} />
+						<Route path={path.specialty} element={<ListSpecialty />} />
+						<Route path={path.doctor} element={<DoctorList />} />
+						<Route path="/handbooks/:id" element={<DetailHandBook />} />
+						<Route path="/doctors/:id" element={<DetailDoctor />} />
+						<Route
+							path="/verify-book-appointment"
+							element={<VerifyEmailBooking />}
+						/>
+					</Route>
+
+					<Route path="/system" element={<DashBoardLayout />}>
+						<Route index element={<DashBoard />} />
+						<Route path="admin/manage-user" element={<ManageUser />} />
+						<Route path="admin/manage-doctor" element={<ManageDoctor />} />
+						<Route path="admin/list-user" element={<ListUser />} />
+						<Route path="admin/manage-clinic" element={<ManageClinic />} />
+						<Route
+							path="admin/manage-specialty"
+							element={<ManageSpecialty />}
+						/>
+						<Route path="admin/manage-handbook" element={<ManageHandBook />} />
+						<Route path="doctor/manage-schedule" element={<ManageSchedule />} />
+						<Route path="doctor/manage-patient" element={<ManagePatient />} />
+					</Route>
+
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Suspense>
 
 			<ToastContainer
 				position="top-right"
