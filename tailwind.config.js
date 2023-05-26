cd2ee5d2096212b5327de2744def648e38405b8d
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-26 17:18:47 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/flowbite/**/*.js",
+	],
 	theme: {
 		extend: {
+			fontFamily: {
+				body: ["Montserrat", "sans-serif"],
+			},
+
 			colors: {
-				primary: "#1266dd",
-				secondary: "#f73859",
-				green: "#13bb7b",
-				orange: "#febb02",
-				bgGray: "#f5f5f5",
+				primary: "#45c3d2",
+				bgPrimary: "#f5f5f5",
+				bgGray: "#eeeeee",
 			},
 		},
-		// backgroundColor: {
-		// 	primary: "#1266dd",
-		// 	secondary: "#f73859",
-		// },
 	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("@tailwindcss/line-clamp")],
+	plugins: [require("flowbite/plugin"), require("@tailwindcss/line-clamp")],
 };
