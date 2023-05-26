6176ff5211ea2ec876a1cf2741c5609809fac6c4
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-26 17:20:15 +0700

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-
 import {BrowserRouter} from "react-router-dom";
+import "./i18n";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+import {persistor, store} from "./redux/store";
+import {I18nextProvider} from "react-i18next";
+import i18n from "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<I18nextProvider i18n={i18n}>
+						<App />
+					</I18nextProvider>
+				</PersistGate>
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
