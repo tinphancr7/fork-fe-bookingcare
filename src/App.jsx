4903ef9947047f6cc6cee63777a271b9fc29d371
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-26 18:03:42 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import ManageUser from "./system/admin/ManageUser";
 // import ManageDoctor from "./system/admin/ManageDoctor";
 import DashBoard from "./system/admin/DashBoard";
 // import ManageSchedule from "./system/doctor/ManageSchedule";
-// import ManageSpecialty from "./system/admin/ManageSpecialty";
+import ManageSpecialty from "./system/admin/ManageSpecialty";
 import ListUser from "./system/admin/ListUser";
-// import ManageClinic from "./system/admin/ManageClinic";
+import ManageClinic from "./system/admin/ManageClinic";
 // import ManagePatient from "./system/doctor/ManagePatient";
 import DashBoardLayout from "./layouts/DashBoardLayout";
 // import ManageHandBook from "./system/admin/ManageHandBook";
@@ -61,12 +61,12 @@ function App() {
 						<Route path="admin/manage-user" element={<ManageUser />} />
 						{/* <Route path="admin/manage-doctor" element={<ManageDoctor />} /> */}
 						<Route path="admin/list-user" element={<ListUser />} />
-						{/* <Route path="admin/manage-clinic" element={<ManageClinic />} /> */}
-						{/* <Route
+						<Route path="admin/manage-clinic" element={<ManageClinic />} />
+						<Route
 							path="admin/manage-specialty"
 							element={<ManageSpecialty />}
 						/>
-						<Route path="admin/manage-handbook" element={<ManageHandBook />} />
+						{/* <Route path="admin/manage-handbook" element={<ManageHandBook />} />
 						<Route path="doctor/manage-schedule" element={<ManageSchedule />} />
 						<Route path="doctor/manage-patient" element={<ManagePatient />} /> */}
 					</Route>
