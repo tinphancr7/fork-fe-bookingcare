337201885f85aa2b0acc46eaea1b1e843a528b5d
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-26 16:10:12 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {PersistGate} from "redux-persist/integration/react";
 import {persistor, store} from "./redux/store";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n";
-import {Formik} from "formik";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
